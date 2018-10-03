90e300f946fdf68f2868e5e4db899e7f2f5687e2
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-10-03 14:41:27 -0700

    Added animal class, updated database, made one more slight change to PW requirement.

---
+++ HometownZoo/Migrations/201810032118526_AddedAnimal.cs
@@ -0,0 +1,16 @@
+namespace HometownZoo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedAnimal : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
